32a2c956f6e31d7911064bf1974fca6f17607ebe
deanljohnson <dean@deanljohnson.com>
2015-06-23 13:34:55 -0700

    Added progress management to Quests
    Users can now assign an actionFinishedCallback that willbe called any
    time an action is finished and a questFinishedCallback.
    
    Quests now have a completionPercentage() method.

--- Quest.js
+++ Quest.js
@@ -6,13 +6,37 @@ var QUESTIFY = (function (QUESTIFY) {
 	"use strict";
 	function createQuest (actions, argumentsArr) {
 		var that = {},
-			finishedMap = new Array(actions.length);
+			finishedMap = new Array(actions.length),
+			actionFinishedCallback = function () {},
+			questFinishedCallback = function () {};
 
 		(function initializeFinishedMap() {
 			for (var i = 0, il = finishedMap.length; i < il; i++) {
 				finishedMap[i] = false;
 			}
 		}());
+
+		function reportQuestFinished() {
+			questFinishedCallback();
+		}
+
+		function reportActionFinished(finishedAction, args) {
+			actionFinishedCallback(finishedAction, args);
+		}
+
+		function isFinished() {
+			return finishedMap.indexOf(false) === -1;
+		}
+
+		function completionPercentage() {
+			var finishedCount = 0;
+
+			for (var f = 0, fl = finishedMap.length; f < fl; f++) {
+				if (finishedMap[f] === true) { finishedCount++; }
+			}
+
+			return (finishedMap.length !== 0) ? finishedCount/finishedMap.length : 0;
+		}
 
 		that.actions = actions;
 		that.argumentsArr = argumentsArr;
@@ -25,11 +49,17 @@ var QUESTIFY = (function (QUESTIFY) {
 					finishedMap[a] = (action.update(argumentsArr[a]) === true);
 
 					if (finishedMap[a] === false) { return; }
+					if (finishedMap[a] === true) { reportActionFinished(action, argumentsArr[a]); }
 				}
 			}
+
+			if (isFinished()) { reportQuestFinished(); }
 		};
 
-		that.isFinished = function() { return finishedMap.indexOf(false) === -1; };
+		that.isFinished = isFinished;
+		that.completionPercentage = completionPercentage;
+		that.actionFinishedCallback = actionFinishedCallback;
+		that.questFinishedCallback = questFinishedCallback;
 
 		return that;
 	}
