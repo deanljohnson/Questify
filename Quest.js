39e8ebb348280795133c6a318de6731f9bce9a2d
deanljohnson <dean@deanljohnson.com>
2015-06-23 00:38:02 -0700

    Simplified. Passing Args to Actions
    Simplified Action update process. Quest is now responsible for tracking
    the completion state of all actions, not the actions themselves. The
    Quest class also passes data to the actions.

--- Quest.js
+++ Quest.js
@@ -4,28 +4,30 @@
 
 var QUESTIFY = (function (QUESTIFY) {
 	"use strict";
-	function createQuest (actions, variables) {
+	function createQuest (actions, argumentsArr) {
 		var that = {},
-			finished = false;
+			finishedMap = new Array(actions.length);
 
-		that.variables = variables;
+		(function initializeFinishedMap() {
+			for (var i = 0, il = finishedMap.length; i < il; i++) {
+				finishedMap[i] = false;
+			}
+		}());
+
+		that.actions = actions;
+		that.argumentsArr = argumentsArr;
 
 		that.updateState = function() {
-			var action,
-				anyUnfinished = false;
+			var action;
 			for (var a = 0, al = actions.length; a < al; a++) {
 				action = actions[a];
-				if (!action.isFinished()) {
-					action.update();
-					if (!action.isFinished()) {
-						anyUnfinished = true;
-					}
+				if (finishedMap[a] === false) {
+					finishedMap[a] = (action.update(argumentsArr[a]) === true);
 				}
 			}
-
-			finished = !anyUnfinished;
 		};
-		that.isFinished = function() { return finished; };
+
+		that.isFinished = function() { return finishedMap.indexOf(false) === -1; };
 
 		return that;
 	}
